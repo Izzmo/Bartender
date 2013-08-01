c8cd1070de06ef7feefe34640a59e4fd8646cc8c
Nick <me@izzmo.com>
2013-08-01 14:55:59 -0400

    Moved welcome message into a variable so we can update in the future more easily.

--- bartender.js
+++ bartender.js
@@ -425,6 +425,7 @@ global.bartender = {
       }
     }
   },
+  welcomeMessage: 'Welcome to the Dubstep room! Type /roominfo to see what this room is all about and to learn the rules for getting up on deck. Type /help to learn more about my commands.',
   welcomeList: [],
   welcomeCheck: function(userid) {
     if(this.isMod(userid)) return false;
@@ -550,7 +551,7 @@ global.bartender = {
     if(this.moderation.userBannedFromRoom(d.user[0].userid))
       this.bot.bootUser(d.user[0].userid, "Sorry, you have been permanently banned from this room.");
     else if(this.welcomeCheck(d.user[0].userid) && undefined === init)
-      this.bot.pm('Welcome to the Dubstep room! Type /roominfo to see what this room is all about and to learn the rules for getting up on deck. Type /help to learn more about my commands.', d.user[0].userid);
+      this.bot.pm(this.welcomeMessage, d.user[0].userid);
     this.resetAFK(d.user[0].userid, false);
   },
   deregistered: function(d) {
